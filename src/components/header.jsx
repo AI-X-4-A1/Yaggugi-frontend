6fc0b23a81606a339a60a0e4b0123f91b61b168c
Stjoo0925 <stjoo0925@gmail.com>
2024-11-02 19:36:06 +0900

    style: header style update

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,15 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
-function header() {
+function Header() {
+  const location = useLocation();
+
+  const routeNames = {
+    "/": "I Home",
+    "/suggestion": "I Suggest",
+    "/search": "I Search",
+    "/planning": "I Plan",
+  };
+
+  const routeName = routeNames[location.pathname];
+
   return (
     <div className="header">
-      <Link to="/" className="title">
-        약국이
-      </Link>
+      <div className="logo">
+        <Link to="/" className="title">
+          약국이
+        </Link>
+        <div className="location">{routeName}</div>
+      </div>
+
       <button className="login">로그인</button>
     </div>
   );
 }
 
-export default header;
+export default Header;
