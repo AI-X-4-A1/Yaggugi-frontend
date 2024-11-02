783f85d184183a5203476641267a3ba6dcb7a629
Stjoo0925 <stjoo0925@gmail.com>
2024-11-02 18:53:20 +0900

    fix: header style fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function header() {
-    return (
-        <div className="header" style={{ fontSize: 36, textAlign: "left" }}>
-            약국이
-        </div>
-    )
+  return (
+    <div className="header">
+      <Link to="/" className="title">
+        약국이
+      </Link>
+      <button className="login">로그인</button>
+    </div>
+  );
 }
 
 export default header;
